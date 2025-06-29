34e4b5926d123d715e9740330cda2dbde54e6b95
myguccicat <tattyronda@gmail.com>
2025-06-30 00:33:55 +0800

    0620_class_35:21_N5.

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -64,6 +64,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "libraries": {
+                "markdown_tag": "libraries.filter.markdown_filter",  # Custom template filter for Markdown
+            },
         },
     },
 ]
